c83e6e9f057489c5fc8ce731cf20357558b715f0
Jared Bass <jaredjbass@gmail.com>
2021-09-05 19:44:54 -0400

    Refactor to new name

--- src/main/java/com/trailblazer/audio/announcement/TrailblazerAudioAnnouncementPlugin.java
+++ src/main/java/com/trailblazer/audio/announcement/TrailblazerAudioAnnouncementPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.trailblazer.audio.announcement;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class TrailblazerAudioAnnouncementPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private TrailblazerAudioAnnouncementConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	TrailblazerAudioAnnouncementConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(TrailblazerAudioAnnouncementConfig.class);
 	}
 }
