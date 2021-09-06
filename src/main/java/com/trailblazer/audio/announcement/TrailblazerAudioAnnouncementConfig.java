b5cf397dd7a9e8d51ac492a37f5c5473877b45a3
Jared Bass <jaredjbass@gmail.com>
2021-09-05 20:02:48 -0400

    Updated config

--- src/main/java/com/trailblazer/audio/announcement/TrailblazerAudioAnnouncementConfig.java
+++ src/main/java/com/trailblazer/audio/announcement/TrailblazerAudioAnnouncementConfig.java
@@ -4,16 +4,25 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup(TrailblazerAudioAnnouncementConfig.GROUP)
 public interface TrailblazerAudioAnnouncementConfig extends Config
 {
+	String GROUP = "trailblazeraudioannouncement";
+
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		keyName = "playCombatAchievement",
+		name = "Combat Achievements",
+		description = "Should the jingle play when you complete a combat achievement?",
+		position = 0
 	)
-	default String greeting()
-	{
-		return "Hello";
-	}
+	default boolean playCombatAchievement() { return true; }
+
+	@ConfigItem(
+			keyName = "playCollectionLog",
+			name = "Collection Log Slot",
+			description = "Should the jingle play when you fill in a collection log slot?",
+			position = 1
+	)
+	default boolean playCollectionLog() { return true; }
+
 }
